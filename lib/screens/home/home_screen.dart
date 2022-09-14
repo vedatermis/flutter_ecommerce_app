3a5d3808acb6e0d1fe0cffd309942a5baffd83a7
Vedat ERMİŞ <vedatermis@hotmail.com>
2022-09-14 21:32:18 +0300

    product carousel

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:ecommerce_app/models/category_model.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/models.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -17,16 +18,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Zero To Unicorn'),
       bottomNavigationBar: const CustomNavBar(),
-      body: Container(
-        child: CarouselSlider(
-          options: CarouselOptions(
-            aspectRatio: 2.0,
-            viewportFraction: 0.9,
-            enlargeCenterPage: true,
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
+      body: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 2.0,
+              viewportFraction: 0.9,
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.height,
+            ),
+            items: Category.categories.map((category) => HeroCarouselCard(category: category)).toList(),
           ),
-          items: Category.categories.map((category) => HeroCarouselCard(category: category)).toList(),
-        ),
+          const SectionTitle(title: 'RECOMMENDED'),
+          ProductCarousel(products: Product.products.where((product) => product.isRecommended).toList()),
+          const SectionTitle(title: 'MOST POPULAR'),
+          ProductCarousel(products: Product.products.where((product) => product.isPopular).toList())
+        ],
       ),
     );
   }
