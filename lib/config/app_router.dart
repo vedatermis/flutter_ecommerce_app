3a6b57d021cff412ceef486b5d316ccb8a71fe22
Vedat ERMİŞ <vedatermis@hotmail.com>
2022-09-16 16:45:59 +0300

    ProductScreen

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,11 +1,10 @@
-import 'package:ecommerce_app/models/category_model.dart';
 import 'package:ecommerce_app/screens/screens.dart';
 import 'package:flutter/material.dart';
 
+import '../models/models.dart';
+
 class AppRouter {
   static Route onGenerateRoute(RouteSettings settings) {
-    print('This is route: ${settings.name}');
-
     switch (settings.name) {
       case '/':
         return HomeScreen.route();
@@ -14,7 +13,7 @@ class AppRouter {
         return CartScreen.route();
 
       case ProductScreen.routeName:
-        return ProductScreen.route();
+        return ProductScreen.route(product: settings.arguments as Product);
 
       case CatalogScreen.routeName:
         return CatalogScreen.route(category: settings.arguments as Category);
--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -1,20 +1,126 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
+import '../../models/models.dart';
+
 class ProductScreen extends StatelessWidget {
+  final Product product;
   static const String routeName = '/product';
 
-  static Route route() {
-    return MaterialPageRoute(settings: const RouteSettings(name: routeName), builder: (_) => const ProductScreen());
+  static Route route({required Product product}) {
+    return MaterialPageRoute(settings: const RouteSettings(name: routeName), builder: (_) => ProductScreen(product: product));
   }
 
-  const ProductScreen({Key? key}) : super(key: key);
+  const ProductScreen({Key? key, required this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Product'),
-      bottomNavigationBar: CustomNavBar(),
+    return Scaffold(
+      appBar: CustomAppBar(title: product.name),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.share,
+                    color: Colors.white,
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  child: Text(
+                    'ADD TO CART',
+                    style: Theme.of(context).textTheme.headline3,
+                  ))
+            ],
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 2.0,
+              viewportFraction: 0.9,
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.height,
+            ),
+            items: [
+              HeroCarouselCard(
+                product: product,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  alignment: Alignment.bottomCenter,
+                  color: Colors.black.withAlpha(50),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 50,
+                  color: Colors.black,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(product.name, style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                        Text('${product.price}', style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: ExpansionTile(
+              initiallyExpanded: true,
+              title: Text('Product Information', style: Theme.of(context).textTheme.headline3),
+              children: [
+                ListTile(
+                  title: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                      style: Theme.of(context).textTheme.bodyText1),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: ExpansionTile(
+              title: Text('Delivery Information', style: Theme.of(context).textTheme.headline3),
+              children: [
+                ListTile(
+                  title: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                      style: Theme.of(context).textTheme.bodyText1),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
