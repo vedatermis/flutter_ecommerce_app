4e03cb7c171fef292f4906d1827ad6dc3e73e9ac
Vedat ERMİŞ <vedatermis@hotmail.com>
2022-09-20 11:51:58 +0300

    Cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/models/product_model.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -12,9 +13,107 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Cart'),
-      bottomNavigationBar: CustomNavBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Cart'),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  child: Text(
+                    'GO TO CHECKOUT',
+                    style: Theme.of(context).textTheme.headline3,
+                  ))
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Add \$20.0 for FREE delivery',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.black, shape: const RoundedRectangleBorder(), elevation: 0),
+                        child: Text('Add More Items', style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)))
+                  ],
+                ),
+                const SizedBox(height: 10),
+                CartProductCard(product: Product.products[0]),
+                CartProductCard(product: Product.products[2]),
+                CartProductCard(product: Product.products[3]),
+              ],
+            ),
+            Column(
+              children: [
+                const Divider(thickness: 2),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('SUBTOTAL', style: Theme.of(context).textTheme.headline5),
+                          Text('\$5.98', style: Theme.of(context).textTheme.headline5),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('DELIVERY FEE', style: Theme.of(context).textTheme.headline5),
+                          Text('\$2.90', style: Theme.of(context).textTheme.headline5),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      decoration: BoxDecoration(color: Colors.black.withAlpha(50)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.all(5.0),
+                      height: 50,
+                      decoration: const BoxDecoration(color: Colors.black),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('TOTAL', style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                            Text('\$12.90', style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
